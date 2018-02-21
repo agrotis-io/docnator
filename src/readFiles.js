de5da6b955e5b819f1d33ad3ee4c0529814a41a6
jhserodio <jhserodio@gmail.com>
2018-02-21 14:22:06 -0300

    feat: create readAllFiles create the function readAllFiles

--- src/readFiles.js
+++ src/readFiles.js
@@ -1,6 +1,6 @@
 const series = require('async-array-methods').series;
 const jetpack = require('fs-jetpack');
-const path = require('path')
+const path = require('path');
 
 /**
  * @function readAllFiles read all js and jsx extension files of diretory(src) and your sub directories.
@@ -13,22 +13,20 @@ async function readAllFiles(src, regExtensions = /(\.js$|\.jsx$)/) {
 
   let files = tree.children.filter(item => {
     return item.type === 'file' && regExtensions.test(item.name);
-  })
+  });
 
-  const dirs = tree.children.filter(item => item.type === 'dir')
+  const dirs = tree.children.filter(item => item.type === 'dir');
 
   if (dirs.length !== 0) {
     await series(dirs, async dir => {
-      const newPath = path.resolve(src, dir.name)
+      const newPath = path.resolve(src, dir.name);
 
-      return readAllFiles(newPath)
-        .then(resp => resp);
-    })
-    .then(resp => {
+      return readAllFiles(newPath).then(resp => resp);
+    }).then(resp => {
       resp.forEach(item => {
-        files.push(...item)
-      })
-    })
+        files.push(...item);
+      });
+    });
   }
 
   return files;
