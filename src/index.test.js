78d778f8f98d6c0eb075f84d97058fd6fe3d78a4
jhserodio <jhserodio@gmail.com>
2018-02-21 14:14:44 -0300

    fix: resolve conflict conflict of stash changes

--- src/index.test.js
+++
@@ -1,8 +0,0 @@
-import sayHello from '../src';
-
-describe('sayHello', () => {
-  it('returns hello', () => {
-    expect(sayHello()).toBe('getting Start-t, agrotis!');
-    expect(sayHello('world')).toBe('getting Start-t, world!');
-  });
-});
