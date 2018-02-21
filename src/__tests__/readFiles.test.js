6fb34607df06bb56f9a7332d404314671c71b2b7
jhserodio <jhserodio@gmail.com>
2018-02-21 16:18:51 -0300

    test: include mock in readFiles.test include the mock structure for tests in readFiles.test

--- src/__tests__/readFiles.test.js
+++ src/__tests__/readFiles.test.js
@@ -5,27 +5,37 @@ const path = require('path');
 
 describe('readFiles', () => {
   //prepare for the tests
-  const summaryPathTest = path.resolve(__dirname, 'src_mock');
+  const srcMock = path.resolve(__dirname, 'src_mock');
+  const { write } = jetpack;
 
-  test('return list of 12 files as js and jsx extension', async () => {
-  });
+  write(`${srcMock}/firstJs.js`, '');
+  write(`${srcMock}/secondJs.js`, '');
+  write(`${srcMock}/thirdJs.js`, '');
+  write(`${srcMock}/firstCss.css`, '');
+  write(`${srcMock}/firstComponent/FirstJsx.jsx`, '');
+  write(`${srcMock}/firstComponent/SecondJsx.jsx`, '');
+  write(`${srcMock}/firstComponent/fourthJs.js`, '');
+  write(`${srcMock}/firstComponent/secondCs.css`, '');
+  write(`${srcMock}/secondComponents/fifthJs.js`, '');
+  write(`${srcMock}/secondComponents/sixthJs.js`, '');
+  write(`${srcMock}/secondComponents/child/ThirdJsx.jsx`, '');
+  write(`${srcMock}/secondComponents/child/seventhJs.js`, '');
+  write(`${srcMock}/secondComponents/child/thirdCss.css`, '');
+  write(`${srcMock}/secondComponents/child/eighthJs.js`, '');
+  write(`${srcMock}/secondComponents/child/ninethJs.js`, '');
 
-  test('return [{name: String, type: String, size: Number}] formater ', async () => {
-  });
 
-  test('return list of 3 files as jsx extension', async () => {
-  });
+  test('return list of 12 files as js and jsx extension', async () => {});
 
-  test('src path dont exist', async () => {
-  });
+  test('return [{name: String, type: String, size: Number}] formater ', async () => {});
 
-  test('src directory is empty', async () => {
-  });
+  test('return list of 3 files as jsx extension', async () => {});
 
-  test('src path dont exist', async () => {
-  });
+  test('src path dont exist', async () => {});
 
-  test('regExtension invali as a regExp Object', async () => {
-  });
+  test('src directory is empty', async () => {});
 
+  test('regExtension invali as a regExp Object', async () => {});
+
+  jetpack.remove(srcMock);
 });
