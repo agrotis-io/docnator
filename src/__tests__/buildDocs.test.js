1763cf845dfc008f0373c48b23fc24d0ab4e6e26
jhserodio <jhserodio@gmail.com>
2018-02-26 18:18:30 -0300

    test: buildDocs unit tests cases
    create the cases of tests in buildDocs.test for buildDocs and mountTemplateDataFileName funcions

--- src/__tests__/buildDocs.test.js
+++ src/__tests__/buildDocs.test.js
@@ -1,11 +1,83 @@
 import { buildDocs } from '../buildDocs';
 
-// describe('buildDocs', () => {
-//   it('return the build result', () => {
-//     const target = 'imRootDirectory';
-//     const extension = 'imJsxExtension';
-//     const result = `this is the source files directory ${target} \n this is the extension ${extension}`;
+describe('buildDocs', () => {
+  // mock
 
-//     expect(buildDocs(target, extension)).toBe(result);
-//   });
-// });
+  const tempDist = path.resolve(process.cwd(), 'temp');
+  const srcMock = path.resolve(tempDist, 'src_mock');
+  const src = '';
+  const fakeSrcPath = './fakePath';
+  const extension = '';
+  const fakeExtension = '';
+  const { writeAsync } = jetpack;
+
+  /**
+    Mount a envirment with files and extensions, create simple code example with jsdocs
+  */
+  beforeAll(async () => {
+    const content = {
+      firstJs: ``,
+      secondJs: ``,
+      thirdJs: ``,
+      FirstJsx: ``,
+      SecondJsx: ``,
+      fourthJs: ``,
+      ThirdJsx: ``,
+      fifthJs: ``,
+      sixthJs: ``,
+    };
+
+    await writeAsync(`${srcMock}/firstJs.js`, '');
+    await writeAsync(`${srcMock}/secondJs.js`, '');
+    await writeAsync(`${srcMock}/thirdJs.js`, '');
+    await writeAsync(`${srcMock}/firstComponent/FirstJsx.jsx`, '');
+    await writeAsync(`${srcMock}/firstComponent/SecondJsx.jsx`, '');
+    await writeAsync(`${srcMock}/firstComponent/fourthJs.js`, '');
+    await writeAsync(`${srcMock}/secondComponents/child/ThirdJsx.jsx`, '');
+    await writeAsync(`${srcMock}/secondComponents/child/fifthJs.js`, '');
+    await writeAsync(`${srcMock}/secondComponents/child/sixthJs.js`, '');
+  });
+
+  it('return the build result', () => {
+    expect(1).toBe(1);
+  });
+
+  it('check doc generate', () => {
+    expect(1).toBe(1);
+  });
+
+  it('check doc generate with multiples declarations', () => {
+    expect(1).toBe(1);
+  });
+
+  it('check doc generate in multiples files with same name', () => {
+    expect(1).toBe(1);
+  });
+
+  it('extension invalid as regExp Object', () => {
+    expect(1).toBe(1);
+  });
+
+  it('src dont exist', () => {
+    expect(1).toBe(1);
+  });
+});
+
+describe('mountTemplateDataFileName', () => {
+  // mocks
+  const templateDataSimple = [];
+  const templateDataComplex = [];
+  const templateDataEmpty = [];
+
+  it('return simple template data', () => {
+    expect(1).toBe(1);
+  });
+
+  it('return complex template data', () => {
+    expect(1).toBe(1);
+  });
+
+  it('return empty template data', () => {
+    expect(1).toBe(1);
+  });
+});
